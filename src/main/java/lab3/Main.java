625d45fbeb00377f93f553be1b20a9fd40e449ed
KingTalon23 <talonking2016@outlook.com>
2025-02-04 17:55:23 -0500

    Game is semi-Playable, players can go back and forth, game terminates when the board is full, there is no way to win yet

--- src/main/java/lab3/Main.java
+++ src/main/java/lab3/Main.java
@@ -1,20 +1,26 @@
 package lab3;
 
-import static lab3.ui.Console.boardDisplay;
+import static lab3.ui.Console.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-            boardDisplay();
-        // TODO: Initialize a TicTacToe game with a 3x3 board and two players
-
+    public static void main(String[] args) throws Exception {
+        fillCoordList();
+        fillHashMap();
         while (true) {
-            // TODO: Display the board
-
-            // TODO: Check if the game is over or a draw, and if so display the result and exit
-            
-            // TODO: Get the next move from the player and update the game state
-
+            boardDisplay();
+            move();
         }
     }
+
+    // TODO: Initialize a TicTacToe game with a 3x3 board and two players
+
+    // while (true) {
+    // TODO: Display the board
+
+    // TODO: Check if the game is over or a draw, and if so display the result and exit
+
+    // TODO: Get the next move from the player and update the game state
+
 }
+
--- src/main/java/lab3/ui/Console.java
+++ src/main/java/lab3/ui/Console.java
@@ -1,20 +1,13 @@
 package lab3.ui;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Console {
-    public static String a1 = " ";
-    public static String a2 = " ";
-    public static String a3 = " ";
-    public static String b1 = " ";
-    public static String b2 = " ";
-    public static String b3 = " ";
-    public static String c1 = " ";
-    public static String c2 = " ";
-    public static String c3 = " ";
     public static Integer currentPlayer = 1;
+    static final List<String> availableCoords = new ArrayList<>();
+    public static Map<String, String> tileMap = new HashMap<>();
+    public static boolean endGame = false;
     public static void boardDisplay() {
-
         String boardString = String.format(
                 """
                         - |__1__|__2__|__3__| _ \s
@@ -24,59 +17,95 @@ public class Console {
                         - | --- + --- + --- + -
                         C |  %7$s  |  %8$s  |  %9$s  |
                         - | --- + --- + --- + -
-                        """, a1, a2, a3, b1, b2, b3, c1, c2, c3);
+                        """, tileMap.get("a1"), tileMap.get("a2"), tileMap.get("a3"), tileMap.get("b1"),
+                tileMap.get("b2"), tileMap.get("b3"), tileMap.get("c1"), tileMap.get("c2"), tileMap.get("c3"));
         System.out.printf(boardString);
+        if (endGame) {
+            // this is here so the board gets displayed as full before the termination
+            System.exit(0);
+        }
     }
+
+    public static void fillCoordList() {
+        availableCoords.add("a1");
+        availableCoords.add("a2");
+        availableCoords.add("a3");
+        availableCoords.add("b1");
+        availableCoords.add("b2");
+        availableCoords.add("b3");
+        availableCoords.add("c1");
+        availableCoords.add("c2");
+        availableCoords.add("c3");
+    }
+
+    public static void fillHashMap() {
+        tileMap.put("a1", " ");
+        tileMap.put("a2", " ");
+        tileMap.put("a3", " ");
+        tileMap.put("b1", " ");
+        tileMap.put("b2", " ");
+        tileMap.put("b3", " ");
+        tileMap.put("c1", " ");
+        tileMap.put("c2", " ");
+        tileMap.put("c3", " ");
+    }
+
 
     public static void move() throws Exception {
         Scanner scanner = new Scanner(System.in);
 
-        if (currentPlayer == 1){
+        if (currentPlayer == 1) {
             while (true) {
-                System.out.print("Please Select a Square: ");
+                System.out.print("Player One: Please Select a Square: ");
                 String input = scanner.nextLine();
-
-                //removing spaces and making letters lowercase so verifying should be easier
                 input = input.replaceAll(" ", "").toLowerCase();
-
-                System.out.println("You selected: " + input);
-
-                break;
+                if (checkInput(input)) {
+                    availableCoords.remove(input);
+                    setMove(input, "X");
+                    break;
+                } else {
+                    System.out.print("Tile not available. Please enter new coordinates\n" +
+                            "e.x: a1(top left), b2(middle), c3(bottom right)");
+                }
             }
             currentPlayer++;
-
-        } else if (currentPlayer == 2){
+        } else if (currentPlayer == 2) {
             while (true) {
-                System.out.print("Please Select a Square: ");
+                System.out.print("Player Two: Please Select a Square: ");
                 String input = scanner.nextLine();
                 input = input.replaceAll(" ", "").toLowerCase();
-
-                System.out.println("You selected: " + input);
-
-                break;
+                if (checkInput(input)) {
+                    availableCoords.remove(input);
+                    setMove(input, "O");
+                    break;
+                } else {
+                    System.out.println("\nTile not available. Please enter new coordinates\n" +
+                            "e.x: a1 -> top left, b2 -> middle, c3 -> bottom right");
+                }
             }
             currentPlayer--;
         } else {
             currentPlayer = 1;
             throw new Exception("Unknown Player Turn (return to Player One)");
         }
-        scanner.close();
+
+        if (availableCoords.isEmpty()) {
+            System.out.print("Tie Game!");
+            endGame = true;
+        }
     }
 
+
+    // i'm like 98% sure these go into the game package but i'm just keeping everything in here for now
     public static boolean checkInput(String userInput) {
-        switch (userInput) {
-            case "a1" -> {
-                return true;
-            }
-            case "a2" -> {
-                return true;
-            }
-            case "a3" -> {
-                return true;
-            }
-        }
-        return false;
+        return availableCoords.contains(userInput);
     }
+
+    public static void setMove(String move, String value) {
+        tileMap.put(move, value);
+    }
+
+
     // TODO: create a CLASS method to display a tictactoe board
 
     // TODO: create a CLASS method to get the next move from a player
